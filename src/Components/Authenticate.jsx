53d1ec094ca455b3cade0a0c53c557c2df1e2398
root <root@DESKTOP-K4B994M>
2023-12-29 10:33:37 -0800

    fourth commit

--- src/Components/Authenticate.jsx
+++ src/Components/Authenticate.jsx
@@ -1,17 +1,47 @@
 
-import { useState } from "react";
+import { useState } from "react"
 
-const AuthForm=() =>{
+
+export default function Authenticate({token, setToken}) {
+
+    const [accessMessage,setAccessMessage] = useState(null);
+
+    const [error, setError] = useState(null);
+
+    const [result, setResult] = useState(null)
+
+    async function handleClick(){
+        try {
+            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate',
+            {
+                method:"GET",
+                headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}` 
+              }
+
+            })
+                const results = await response.json();
+                console.log(results)
+                setAccessMessage(results.message)
+
+        } catch (error){
+            setError(error.message);
+        }
+        
+    }
+    
 
     return(
         <div>
-            <form>
-                <h1> Authenticate </h1>
-            </form>
-        </div>
-    )
+        <h2>Authenticate</h2>
+        {accessMessage && <p>{accessMessage}</p>}
+        {error && <p>{error}</p>}
+        <button onClick={handleClick}>Authenticate Token!</button>
+      
+</div>
+);
 }
-export default function Authenticate() {
-    return <h2>Authenticate!</h2>;
-  } 
+  
+
  
--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -1,10 +1,8 @@
 
 import { useState } from "react"
 
+export default function SignUpForm({token, setToken}) {
 
-
-
-export default function SignUpForm() {
     const {userName, setUserName}= useState ("")
     
     const {password, setPassword}= useState ("")
@@ -14,7 +12,28 @@ export default function SignUpForm() {
 async function handleSubmit(event) {
         event.preventDefault()
         //console.log("hello")
+        try{
+            const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/signup`,
+            { 
+                method: "POST", 
+                headers: {
+                    "Content-Type" : " application/json"
+                },
+                    body: JSON.stringify({ 
+                        username: {username}, 
+                        password: {password}
+                })
+            })
+            const result= await response.json();
+            setToken(result.token);
+            console.log(result)
+        }catch(error){
+            setError(error.message);
     }
+
+
+
+}
     return (
         <div> 
             <h2>Sign Up!</h2>
@@ -29,11 +48,6 @@ async function handleSubmit(event) {
                         Password:
                         <input value ={password} onChange={(event)=>{
                             setPassword(event.target.value)
-                         }}
-                         />
-                        <br /><br />
-                         <input value ={error} onChange={(event)=>{   //error space
-                            setError(event.target.value)
                          }}
                          />
                          <br />
