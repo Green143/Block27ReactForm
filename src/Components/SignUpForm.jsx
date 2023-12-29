4cf12eecfc12db1a93f131e3aa27992c79b0f24e
root <root@DESKTOP-K4B994M>
2023-12-29 11:00:11 -0800

    fifth commit

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 
 export default function SignUpForm({token, setToken}) {
 
-    const {userName, setUserName}= useState ("")
+    const {username, setUsername}= useState ("")
     
     const {password, setPassword}= useState ("")
 
@@ -13,45 +13,38 @@ async function handleSubmit(event) {
         event.preventDefault()
         //console.log("hello")
         try{
-            const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/signup`,
+            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',
             { 
                 method: "POST", 
                 headers: {
                     "Content-Type" : " application/json"
                 },
                     body: JSON.stringify({ 
-                        username: {username}, 
-                        password: {password}
+                        username: username, 
+                        password: password
                 })
             })
-            const result= await response.json();
-            setToken(result.token);
-            console.log(result)
-        }catch(error){
-            setError(error.message);
+            const result= await response.json()
+            setToken(result.token)
+            //console.log(result)
+        } catch (error) {
+            setError(error.message)
+        }
     }
-
-
-
-}
     return (
         <div> 
             <h2>Sign Up!</h2>
-                <form onSubmit={(handleSubmit)}>
+                <form onSubmit={handleSubmit}>
                     <label>
                        Username:
-                         <input value= {userName} onChange={(event)=>{
-                            setUserName(event.target.value)
-                         }}
-                        />
-                        <br />
-                        Password:
-                        <input value ={password} onChange={(event)=>{
-                            setPassword(event.target.value)
-                         }}
-                         />
-                         <br />
+                         <input value={username} onChange={(event)=> setUsername(event.target.value)}/>
                     </label>
+                    <label>  <br></br>  
+                        Password:
+                        <input type= "password" value={password} onChange={(event)=> setPassword(event.target.value)}/>
+                    </label>     
+                         <br />
+                  
                 <button type='submit'> Submit </button>
             </form>
         </div>
